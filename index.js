ef22ab83169d89ebe4212f0096f1cbfe56c08f8c
Carly Pecora <carlyannpecora@gmail.com>
2019-03-27 15:48:18 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -10,19 +10,70 @@ function setCart(c) {
 }
 
 function addToCart(item) {
- // write your code here
+  var price = Math.floor(Math.random() * 100)
+  cart.push({["itemName"]: item, ["itemPrice"]: price})
+  return `${item} has been added to your cart.`
 }
 
-function viewCart() {
-  // write your code here
+// function viewCart() {
+//     // First get the total number of keys in
+//     // in the dictionary
+//     var totalKeys = 0
+//     for (var i=0; i<cart.length;i++) {
+//         totalKeys = totalKeys + Object.keys(cart[i]).length
+//     }
+
+//     var cartStr = "In your cart, you have"
+//     var keyCounter = 0
+//     for (var i=0;i<cart.length;i++) {
+//         for (key in cart[i]) {
+//             var newStr = " " + key + " at $" + cart[i][key]
+//             // if we're at the last item
+//             // use an `and`
+//             if (keyCounter === (totalKeys - 1)) {
+//                 newStr = " and" + newStr + "."
+//             } else {
+//                 newStr = newStr + ","
+//             }
+//             cartStr = cartStr + newStr
+//             keyCounter++
+//         }
+//     }
+//     return cartStr
+// }
+// console.log(viewCart())
+
+function viewCart(){
+  var str = "In your cart, you have "
+  for(var i = 0; i < cart.length; i++){
+    var newStr = `${cart[i]["itemName"]} at $${cart[i]["itemPrice"]}`
+    if(i === cart.length - 1){
+      newStr = `, and ${newStr}.`
+    }
+    str = str + newStr
+  }
+  return str
 }
+
+
 
 function total() {
-  // write your code here
+  var counter = 0
+  for(var i = 0; i < cart.length; i++){
+    for(var key in cart[i])
+    counter += cart[i][key]
+  }
+  return counter
 }
 
 function removeFromCart(item) {
-  // write your code here
+  for(var i = 0; i < cart.length; i++){
+    if(cart[i]["itemName"] === item){
+      delete cart[i]
+      return cart
+    }
+  }
+  return "That item is not in your cart."
 }
 
 function placeOrder(cardNumber) {
