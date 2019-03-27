569e1d921f56f2868fcfc6c979727f2a1627dcf2
Carly Pecora <carlyannpecora@gmail.com>
2019-03-27 16:06:08 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -78,6 +78,7 @@ function removeFromCart(item) {
     return
   }
   for(var i = 0; i < cart.length; i++){
+    console.log(cart[i])
     if(cart[i]["itemName"] === item){
       delete cart[i]
       return cart
