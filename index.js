db259a2ed03885993af6e07aca8501b42adbdcc2
Carly Pecora <carlyannpecora@gmail.com>
2019-03-27 16:39:37 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -59,6 +59,7 @@ function placeOrder(cardNumber) {
   if(!cardNumber){
     return "Sorry, we don't have a credit card on file for you."
   }
+  var foo = total()
   cart = []
-  return `Your total cost is $${total()}, which will be charged to the card ${cardNumber}.`
+  return `Your total cost is $${foo}, which will be charged to the card ${cardNumber}.`
 }
